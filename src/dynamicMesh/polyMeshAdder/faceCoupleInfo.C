d99599d34980380231b3ac7540431e3b181f733e
Mark Olesen <Mark.Olesen@Germany>
2008-06-25 15:01:46 +0200

    updated Copyright (C) \d+-2008 OpenCFD Ltd.

--- src/dynamicMesh/polyMeshAdder/faceCoupleInfo.C
+++ src/dynamicMesh/polyMeshAdder/faceCoupleInfo.C
@@ -2,7 +2,7 @@
   =========                 |
   \\      /  F ield         | OpenFOAM: The Open Source CFD Toolbox
    \\    /   O peration     |
-    \\  /    A nd           | Copyright (C) 1991-2007 OpenCFD Ltd.
+    \\  /    A nd           | Copyright (C) 1991-2008 OpenCFD Ltd.
      \\/     M anipulation  |
 -------------------------------------------------------------------------------
 License
