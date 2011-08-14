b689b84477a46ed97f018b67a9f6ae39dadc5b72
Henry <Henry>
2011-08-14 12:17:30 +0100

    Copyright transfered to the OpenFOAM Foundation

--- src/dynamicMesh/polyMeshAdder/polyMeshAdder.C
+++ src/dynamicMesh/polyMeshAdder/polyMeshAdder.C
@@ -2,7 +2,7 @@
   =========                 |
   \\      /  F ield         | OpenFOAM: The Open Source CFD Toolbox
    \\    /   O peration     |
-    \\  /    A nd           | Copyright (C) 2004-2010 OpenCFD Ltd.
+    \\  /    A nd           | Copyright (C) 2011 OpenFOAM Foundation
      \\/     M anipulation  |
 -------------------------------------------------------------------------------
 License
