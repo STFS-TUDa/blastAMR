20e61e6ca66445bcd4682d03f364683e1bdf73ce
mattijs <mattijs>
2015-11-17 15:05:05 +0000

    ENH: parallel: overhaul of parallel mapping
    - redistributePar to have almost (complete) functionality of decomposePar+reconstructPar
    - low-level distributed Field mapping
    - support for mapping surfaceFields (including flipping faces)
    - support for decomposing/reconstructing refinement data

--- applications/utilities/parallelProcessing/reconstructParMesh/reconstructParMesh.C
+++ applications/utilities/parallelProcessing/reconstructParMesh/reconstructParMesh.C
@@ -3,7 +3,7 @@
   \\      /  F ield         | OpenFOAM: The Open Source CFD Toolbox
    \\    /   O peration     |
     \\  /    A nd           | Copyright (C) 2011-2015 OpenFOAM Foundation
-     \\/     M anipulation  |
+     \\/     M anipulation  | Copyright (C) 2015 OpenCFD Ltd.
 -------------------------------------------------------------------------------
 License
     This file is part of OpenFOAM.
@@ -899,7 +899,7 @@ int main(int argc, char *argv[])
     }
 
 
-    Info<< "End.\n" << endl;
+    Info<< "\nEnd\n" << endl;
 
     return 0;
 }
