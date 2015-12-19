0009d6b4e94501df05958c3f993c1908906b4421
mattijs <mattijs>
2015-12-19 14:22:30 +0000

    ENH: copyright: make consistent with OpenFOAM-history

--- applications/utilities/parallelProcessing/reconstructParMesh/reconstructParMesh.C
+++ applications/utilities/parallelProcessing/reconstructParMesh/reconstructParMesh.C
@@ -3,7 +3,7 @@
   \\      /  F ield         | OpenFOAM: The Open Source CFD Toolbox
    \\    /   O peration     |
     \\  /    A nd           | Copyright (C) 2011-2015 OpenFOAM Foundation
-     \\/     M anipulation  | Copyright (C) 2015 OpenCFD Ltd.
+     \\/     M anipulation  |
 -------------------------------------------------------------------------------
 License
     This file is part of OpenFOAM.
