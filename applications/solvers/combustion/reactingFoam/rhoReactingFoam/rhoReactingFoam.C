cb7cc8c219d72d0903d5bc766d089144b7aa9268
Mark Olesen <Mark.Olesen@esi-group.com>
2018-04-27 15:00:34 +0200

    ENH: make format of ExecutionTime = ... output configurable (issue #788)
    - controlled by the the 'printExecutionFormat' InfoSwitch in
      etc/controlDict
    
          // Style for "ExecutionTime = " output
          // - 0 = seconds (with trailing 's')
          // - 1 = day-hh:mm:ss
    
       ExecutionTime = 112135.2 s  ClockTime = 113017 s
    
       ExecutionTime = 1-07:08:55.20  ClockTime = 1-07:23:37
    
    - Callable via the new Time::printExecutionTime() method,
      which also helps to reduce clutter in the applications.
      Eg,
    
         runTime.printExecutionTime(Info);
    
      vs
    
         Info<< "ExecutionTime = " << runTime.elapsedCpuTime() << " s"
             << "  ClockTime = " << runTime.elapsedClockTime() << " s"
             << nl << endl;
    
    --
    
    ENH: return elapsedClockTime() and clockTimeIncrement as double
    
    - previously returned as time_t, which is less portable.

--- applications/solvers/combustion/reactingFoam/rhoReactingFoam/rhoReactingFoam.C
+++ applications/solvers/combustion/reactingFoam/rhoReactingFoam/rhoReactingFoam.C
@@ -121,9 +121,7 @@ int main(int argc, char *argv[])
 
         runTime.write();
 
-        Info<< "ExecutionTime = " << runTime.elapsedCpuTime() << " s"
-            << "  ClockTime = " << runTime.elapsedClockTime() << " s"
-            << nl << endl;
+        runTime.printExecutionTime(Info);
     }
 
     Info<< "End\n" << endl;
