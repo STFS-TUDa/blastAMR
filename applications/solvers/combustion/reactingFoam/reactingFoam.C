8d4dee739908366e44f9ad6e868cda4a2daec070
Mark Olesen <Mark.Olesen@esi-group.com>
2018-07-02 10:20:01 +0200

    ENH: improve controls for Time (issue #910)
    - relocate some standard functionality to TimePaths to allow a lighter
      means of managing time directories without using the entire Time
      mechanism.
    
    - optional enableLibs for Time construction (default is on)
      and a corresponding argList::noLibs() and "-no-libs" option
    
    STYLE:
    
    - mark Time::outputTime() as deprecated MAY-2016
    
    - use pre-increment for runTime, although there is no difference in
      behaviour or performance.

--- applications/solvers/combustion/reactingFoam/reactingFoam.C
+++ applications/solvers/combustion/reactingFoam/reactingFoam.C
@@ -85,7 +85,7 @@ int main(int argc, char *argv[])
             #include "setDeltaT.H"
         }
 
-        runTime++;
+        ++runTime;
 
         Info<< "Time = " << runTime.timeName() << nl << endl;
 
--- applications/solvers/combustion/reactingFoam/rhoReactingFoam/rhoReactingFoam.C
+++ applications/solvers/combustion/reactingFoam/rhoReactingFoam/rhoReactingFoam.C
@@ -87,7 +87,7 @@ int main(int argc, char *argv[])
             #include "setDeltaT.H"
         }
 
-        runTime++;
+        ++runTime;
 
         Info<< "Time = " << runTime.timeName() << nl << endl;
 
