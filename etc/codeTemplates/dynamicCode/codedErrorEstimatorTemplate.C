1648fd356bb380d63c49c8a325b06359280b390f
Elwardi <elwardi.fadeli@tu-darmstadt.de>
2023-01-29 20:03:36 +0100

    DEV: Add coded errorEstimator for AMR operations

---
+++ etc/codeTemplates/dynamicCode/codedErrorEstimatorTemplate.C
@@ -0,0 +1,143 @@
+/*---------------------------------------------------------------------------*\
+  =========                 |
+  \\      /  F ield         | OpenFOAM: The Open Source CFD Toolbox
+   \\    /   O peration     |
+    \\  /    A nd           | www.openfoam.com
+     \\/     M anipulation  |
+-------------------------------------------------------------------------------
+    Copyright (C) 2019 OpenCFD Ltd.
+    Copyright (C) YEAR AUTHOR, AFFILIATION
+-------------------------------------------------------------------------------
+License
+    This file is part of OpenFOAM.
+
+    OpenFOAM is free software: you can redistribute it and/or modify it
+    under the terms of the GNU General Public License as published by
+    the Free Software Foundation, either version 3 of the License, or
+    (at your option) any later version.
+
+    OpenFOAM is distributed in the hope that it will be useful, but WITHOUT
+    ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or
+    FITNESS FOR A PARTICULAR PURPOSE.  See the GNU General Public License
+    for more details.
+
+    You should have received a copy of the GNU General Public License
+    along with OpenFOAM.  If not, see <http://www.gnu.org/licenses/>.
+
+\*---------------------------------------------------------------------------*/
+
+#include "codedErrorEstimatorTemplate.H"
+#include "fvCFD.H"
+#include "unitConversion.H"
+#include "addToRunTimeSelectionTable.H"
+
+// * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * //
+
+namespace Foam
+{
+namespace errorEstimators
+{
+
+// * * * * * * * * * * * * * * Static Data Members * * * * * * * * * * * * * //
+
+defineTypeNameAndDebug(${typeName}ErrorEstimator, 0);
+
+addRemovableToRunTimeSelectionTable
+(
+    errorEstimator,
+    ${typeName}ErrorEstimator,
+    dictionary
+);
+
+
+// * * * * * * * * * * * * * * * Global Functions  * * * * * * * * * * * * * //
+
+// dynamicCode:
+// SHA1 = ${SHA1sum}
+//
+// unique function name that can be checked if the correct library version
+// has been loaded
+extern "C" void ${typeName}_${SHA1sum}(bool load)
+{
+    if (load)
+    {
+        // Code that can be explicitly executed after loading
+    }
+    else
+    {
+        // Code that can be explicitly executed before unloading
+    }
+}
+
+
+// * * * * * * * * * * * * * * * Local Functions * * * * * * * * * * * * * * //
+
+//{{{ begin localCode
+${localCode}
+//}}} end localCode
+
+
+// * * * * * * * * * * * * * Private Member Functions  * * * * * * * * * * * //
+
+const fvMesh& ${typeName}ErrorEstimator::mesh() const
+{
+    return mesh_;
+}
+
+
+// * * * * * * * * * * * * * * * * Constructors  * * * * * * * * * * * * * * //
+
+${typeName}ErrorEstimator::${typeName}ErrorEstimator
+(
+    const fvMesh& mesh,
+    const dictionary& dict,
+    const word& name
+)
+:
+    errorEstimator(mesh, dict, name)
+{
+    read(dict);
+}
+
+
+// * * * * * * * * * * * * * * * * Destructor  * * * * * * * * * * * * * * * //
+
+${typeName}ErrorEstimator::~${typeName}ErrorEstimator()
+{}
+
+
+// * * * * * * * * * * * * * * * Member Functions  * * * * * * * * * * * * * //
+
+void ${typeName}ErrorEstimator::read(const dictionary& dict)
+{
+    if (${verbose:-false})
+    {
+        printMessage("read ${typeName}");
+    }
+
+//{{{ begin code
+    ${codeRead}
+//}}} end code
+
+}
+
+
+void ${typeName}ErrorEstimator::update(const bool scale)
+{
+    if (${verbose:-false})
+    {
+        printMessage("update ${typeName}");
+    }
+
+//{{{ begin code
+    ${code}
+//}}} end code
+
+}
+
+// * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * //
+
+}
+} // End namespace Foam
+
+// ************************************************************************* //
---
+++ src/errorEstimators/codedErrorEstimator/codedErrorEstimator.C
@@ -0,0 +1,214 @@
+/*---------------------------------------------------------------------------*\
+  =========                 |
+  \\      /  F ield         | OpenFOAM: The Open Source CFD Toolbox
+   \\    /   O peration     |
+    \\  /    A nd           | www.openfoam.com
+     \\/     M anipulation  |
+-------------------------------------------------------------------------------
+    Copyright (C) 2021 OpenCFD Ltd.
+-------------------------------------------------------------------------------
+License
+    This file is part of OpenFOAM.
+
+    OpenFOAM is free software: you can redistribute it and/or modify it
+    under the terms of the GNU General Public License as published by
+    the Free Software Foundation, either version 3 of the License, or
+    (at your option) any later version.
+
+    OpenFOAM is distributed in the hope that it will be useful, but WITHOUT
+    ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or
+    FITNESS FOR A PARTICULAR PURPOSE.  See the GNU General Public License
+    for more details.
+
+    You should have received a copy of the GNU General Public License
+    along with OpenFOAM.  If not, see <http://www.gnu.org/licenses/>.
+
+\*---------------------------------------------------------------------------*/
+
+#include "dynamicCode.H"
+#include "dynamicCodeContext.H"
+#include "codedErrorEstimator.H"
+#include "addToRunTimeSelectionTable.H"
+
+// * * * * * * * * * * * * * * Static Data Members * * * * * * * * * * * * * //
+
+namespace Foam
+{
+namespace errorEstimators
+{
+    defineTypeNameAndDebug(codedErrorEstimator, 0);
+    addToRunTimeSelectionTable(errorEstimator, codedErrorEstimator, dictionary);
+}
+}
+
+
+// * * * * * * * * * * * * Protected Member Functions  * * * * * * * * * * * //
+
+Foam::dlLibraryTable&
+Foam::errorEstimators::codedErrorEstimator::libs() const
+{
+    return this->time().libs();
+}
+
+
+Foam::string
+Foam::errorEstimators::codedErrorEstimator::description() const
+{
+    return "codedErrorEstimator " + redirectName_;
+}
+
+
+void Foam::errorEstimators::codedErrorEstimator::clearRedirect() const
+{
+    redirectErrorEstimatorPtr_.clear();
+}
+
+
+const Foam::dictionary&
+Foam::errorEstimators::codedErrorEstimator::codeContext() const
+{
+    // What else would make sense?
+    return dict_;
+}
+
+
+const Foam::dictionary&
+Foam::errorEstimators::codedErrorEstimator::codeDict
+(
+    const dictionary& dict
+) const
+{
+    // Use named subdictionary if present to provide the code.
+    // This allows running with multiple Function1s
+
+    return
+    (
+        dict.found("code")
+      ? dict
+      : dict.subDict(redirectName_)
+    );
+}
+
+
+const Foam::dictionary&
+Foam::errorEstimators::codedErrorEstimator::codeDict() const
+{
+    return codeDict(dict_);
+}
+
+
+void Foam::errorEstimators::codedErrorEstimator::prepare
+(
+    dynamicCode& dynCode,
+    const dynamicCodeContext& context
+) const
+{
+    if (context.code().empty())
+    {
+        FatalIOErrorInFunction(dict_)
+            << "No code section in input dictionary for errorEstimator "
+            << " name " << redirectName_
+            << exit(FatalIOError);
+    }
+
+    // Take no chances - typeName must be identical to redirectName_
+    dynCode.setFilterVariable("typeName", redirectName_);
+
+    // Set TemplateType and FieldType filter variables
+    //dynCode.setFieldTemplates<Type>();
+
+    // Compile filtered C template
+    dynCode.addCompileFile(codeTemplateC);
+
+    // Copy filtered H template
+    dynCode.addCopyFile(codeTemplateH);
+
+    #ifdef FULLDEBUG
+    dynCode.setFilterVariable("verbose", "true");
+    DetailInfo
+        <<"compile " << redirectName_ << " sha1: " << context.sha1() << endl;
+    #endif
+
+    // Define Make/options
+    dynCode.setMakeOptions
+    (
+        "EXE_INC = \\\n"
+        "-I$(LIB_SRC)/meshTools/lnInclude \\\n"
+        "-I$(LIB_SRC)/finiteVolume/lnInclude \\\n"
+        "-I/tmp/blastfoam/src/errorEstimators/lnInclude \\\n"
+      + context.options()
+      + "\n\nLIB_LIBS = \\\n"
+        "    -lOpenFOAM \\\n"
+        "    -lfiniteVolume \\\n"
+        "    -lmeshTools \\\n"
+        "    -L$(FOAM_USER_LIBBIN) -lerrorEstimate \\\n"
+      + context.libs()
+    );
+}
+
+
+// * * * * * * * * * * * * * * * * Constructors  * * * * * * * * * * * * * * //
+
+Foam::errorEstimators::codedErrorEstimator::codedErrorEstimator
+(
+    const fvMesh& mesh,
+    const dictionary& dict,
+    const word& name
+)
+:
+    errorEstimator(mesh, dict, name),
+    codedBase(),
+    dict_(dict),
+    redirectName_(dict.getOrDefault<word>("name", name))
+{
+    //this->codedBase::setCodeContext(dict_);
+    read(dict_);
+
+    // No additional code chunks...
+
+    updateLibrary(redirectName_);
+    redirectErrorEstimator();
+}
+
+
+// * * * * * * * * * * * * * * * Member Functions  * * * * * * * * * * * * * //
+
+
+Foam::errorEstimator&
+Foam::errorEstimators::codedErrorEstimator::redirectErrorEstimator()
+{
+    if (!redirectErrorEstimatorPtr_)
+    {
+        dictionary constructDict(dict_);
+        constructDict.set("errorEstimator", redirectName_);
+
+        redirectErrorEstimatorPtr_ = errorEstimator::New
+        (
+            mesh_,
+            constructDict,
+            redirectName_
+        );
+    }
+    return *redirectErrorEstimatorPtr_;
+}
+
+
+void Foam::errorEstimators::codedErrorEstimator::update(const bool scale)
+{
+    // Ensure library containing user-defined code is up-to-date
+    updateLibrary(redirectName_);
+    return redirectErrorEstimator().update(scale);
+}
+
+
+bool Foam::errorEstimators::codedErrorEstimator::writeData
+(
+    Ostream& os
+) const
+{
+    dict_.writeEntry(this->name(), os);
+    return true;
+}
+
+
+// ************************************************************************* //
