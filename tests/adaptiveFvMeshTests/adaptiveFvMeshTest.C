ff1e94ad29f90058fb318b2445c89f122f85b24a
Elwardi <fadeli.elwardi@tu-darmstadt.de>
2023-11-27 10:03:52 +0100

    test: add unit tests for protected patch cells
    works on #4

--- tests/adaptiveFvMeshTests/adaptiveFvMeshTest.C
+++ tests/adaptiveFvMeshTests/adaptiveFvMeshTest.C
@@ -10,6 +10,7 @@
 #include "volFieldsFwd.H"
 #include "boxToCell.H"
 #include "cellSet.H"
+#include "stringOps.H"
 
 #include <csetjmp>
 #include <csignal>
@@ -33,7 +34,7 @@ void onSigabrt(int signum)
 }
 void tryAndCatchAbortingCode(std::function<void(void)> func)
 {
-    FatalError.throwExceptions();
+    FatalError.dontThrowExceptions();
     if (setjmp (amr_env) == 0) {
         signal(SIGUSR1, &onSigabrt);
         signal(SIGUSR2, &onSigabrt);
@@ -58,7 +59,11 @@ void tryAndCatchAbortingCode(std::function<void(void)> func)
 }
 // ********************************************************************************************** //
 
-TEST_CASE("Check polyhedral AMR/LB functionality for adaptiveFvMesh", "[hex2D][hex3D][poly2D][poly3D][serial][parallel][!throws]")
+TEST_CASE
+(
+    "Check refinement/unrefinement polyhedral AMR/LB functionality for adaptiveFvMesh",
+    "[hex2D][hex3D][poly2D][poly3D][serial][parallel][!throws]"
+)
 {
     FatalError.throwExceptions();
     Time& runTime = *timePtr;
@@ -71,14 +76,15 @@ TEST_CASE("Check polyhedral AMR/LB functionality for adaptiveFvMesh", "[hex2D][h
     // Tested variables' matrix 
     word refiner = GENERATE("polyRefiner");
     word balance = Pstream::parRun() ? GENERATE("no", "yes") : GENERATE("no");
+    label nBufferLayers = GENERATE(1, 2);
     label maxRefL = GENERATE(1, 3);
 
     // Supported constant/dynamicMeshDict entries
     IStringStream is
     (
         "dynamicFvMesh   adaptiveFvMesh;"
-        "errorEstimator  delta;"
-        "deltaField      test;"
+        "errorEstimator  fieldValue;"
+        "fieldName       test;"
         "balance         "+balance+";"
         "refiner         "+refiner+";"
         "refineInterval  1;"
@@ -86,233 +92,259 @@ TEST_CASE("Check polyhedral AMR/LB functionality for adaptiveFvMesh", "[hex2D][h
         "balanceInterval  1;"
         "lowerRefineLevel 0.01;"
         "unrefineLevel 0.01;"
-        "nBufferLayers   3;"
+        "nBufferLayers   "+Foam::name(nBufferLayers)+";"
         "maxRefinement   "+Foam::name(maxRefL)+";"
         "dumpLevel       false;"
+        "protectedPatches ();"
+        "nPatchesBuffers 1;"
     );
 
-    SECTION("Box is refined incrementally " + Foam::name(maxRefL) + " times")
-    {
-        // Reset time
-        runTime.setTime(0.0, 0);
+    // Reset time
+    runTime.setTime(0.0, 0);
  
-        // Create mesh object
-        IOdictionary dynamicMeshDict
+    // Create mesh object
+    IOdictionary dynamicMeshDict
+    (
+        IOobject
         (
-            IOobject
-            (
-                "dynamicMeshDict",
-                runTime.constant(),
-                runTime,
-                IOobject::NO_READ,
-                IOobject::NO_WRITE
-            ),
-            is
-        );
-        dynamicMeshDict.regIOobject::write();
-        adaptiveFvMesh mesh
+            "dynamicMeshDict",
+            runTime.constant(),
+            runTime,
+            IOobject::NO_READ,
+            IOobject::NO_WRITE
+        ),
+        is
+    );
+    dynamicMeshDict.regIOobject::write();
+    adaptiveFvMesh mesh
+    (
+        IOobject
         (
-            IOobject
-            (
-                "",
-                runTime.constant(),
-                runTime,
-                IOobject::MUST_READ,
-                IOobject::NO_WRITE
-            )
-        );
+            "",
+            runTime.constant(),
+            runTime,
+            IOobject::MUST_READ,
+            IOobject::NO_WRITE
+        )
+    );
 
-        // Capture test variables for sane reports
-        CAPTURE
-        (
-            Pstream::myProcNo(), Pstream::nProcs(),
-            mesh.nCells(), balance,
-            refiner, maxRefL
-        );
+    // Capture test variables for sane reports
+    CAPTURE
+    (
+        Pstream::myProcNo(), Pstream::nProcs(),
+        runTime.caseName(),
+        mesh.nCells(), balance,
+        refiner, maxRefL, nBufferLayers
+    );
 
-        // Create AMR criterion field
-        volScalarField test
+    // Create AMR criterion field
+    volScalarField test
+    (
+        IOobject
         (
-            IOobject
-            (
-                "test",
-                runTime.timeName(),
-                mesh,
-                IOobject::NO_READ,
-                IOobject::NO_WRITE,
-                true
-            ),
+            "test",
+            runTime.timeName(),
             mesh,
-            dimensionedScalar("zero",dimless,0.0)
-        );
+            IOobject::NO_READ,
+            IOobject::NO_WRITE,
+            true
+        ),
+        mesh,
+        dimensionedScalar("zero",dimless,0.0)
+    );
 
-        // Set test to 1 inside the refined box
-        forAll(mesh.C(), ci)
+    // Set test to 1 inside the refined box
+    forAll(mesh.C(), ci)
+    {
+        const auto& center = mesh.C()[ci];
+        if 
+        (
+            center.x() >= boxBounds[0] && center.x() <= boxBounds[1]
+            && center.y() >= boxBounds[2] && center.y() <= boxBounds[3]
+        )
         {
-            const auto& center = mesh.C()[ci];
-            if 
-            (
-                center.x() >= boxBounds[0] && center.x() <= boxBounds[1]
-                && center.y() >= boxBounds[2] && center.y() <= boxBounds[3]
-            )
-            {
-                test[ci] = 1;
-            } else {
-                test[ci] = 0;
-            }
+            test[ci] = 1;
+        } else {
+            test[ci] = 0;
         }
-
-        // Cell set for the refined box
-        IStringStream refBoxIs(boxString);
-        boxToCell refBox(mesh, refBoxIs);
-        cellSet refBoxCells(mesh, "refBoxCells", IOobject::NO_READ);
-        refBox.applyToSet(topoSetSource::setAction::NEW, refBoxCells);
-
-        // Record initial number of cells
-        label origRefBoxNCells = returnReduce(refBoxCells.size(), sumOp<label>());
-        label newRefBoxNCells = -1;
-
-        // run a refinement cycle, minor unrefinement should happen here, but this not checked.
-        for(label i = 0; i < maxRefL; i++)
-        {
-            runTime++;
-            // Update mesh
-            tryAndCatchAbortingCode
-            (
-                [&]()
-                {
-                  mesh.update();
-                }
-            );
-        }
-
-        // Check the refined box again with a cellSet
-        IStringStream newRefBoxIs(boxString);
-        boxToCell newRefBox(mesh, newRefBoxIs);
-        cellSet newRefBoxCells(mesh, "newRefBoxCells", IOobject::NO_READ);
-        newRefBox.applyToSet(topoSetSource::setAction::NEW, newRefBoxCells);
-
-        newRefBoxNCells = returnReduce(newRefBoxCells.size(), sumOp<label>());
-
-        // Require that the new box gets refined to at least 4xmaxRefLx the original cell count
-        // This is an inaccurate check; but there is nothing we can do.
-        CHECK(newRefBoxNCells >= 4 * origRefBoxNCells);
     }
 
-    SECTION("Box is unrefined, by moving the refinement region to somewhere else.")
+    // Cell set for the refined box
+    IStringStream refBoxIs(boxString);
+    boxToCell refBox(mesh, refBoxIs);
+    cellSet refBoxCells(mesh, "refBoxCells", IOobject::NO_READ);
+    refBox.applyToSet(topoSetSource::setAction::NEW, refBoxCells);
+
+    // Record initial number of cells
+    label origRefBoxNCells = returnReduce(refBoxCells.size(), sumOp<label>());
+    label newRefBoxNCells = -1;
+
+    // run a refinement cycle, minor unrefinement should happen here, but this not checked.
+    for(label i = 0; i < maxRefL; i++)
     {
-        // Reset time
-        runTime.setTime(0.0, 0);
-
-        // Create the mesh
-        IOdictionary dynamicMeshDict
-        (
-            IOobject
-            (
-                "dynamicMeshDict",
-                runTime.constant(),
-                runTime,
-                IOobject::NO_READ,
-                IOobject::NO_WRITE
-            ),
-            is
-        );
-        dynamicMeshDict.regIOobject::write();
-        adaptiveFvMesh mesh
-        (
-            IOobject
-            (
-                "",
-                runTime.constant(),
-                runTime,
-                IOobject::MUST_READ,
-                IOobject::NO_WRITE
-            )
-        );
-
-        // Capture test variables for sane reports
-        CAPTURE
-        (
-            Pstream::myProcNo(), Pstream::nProcs(),
-            mesh.nCells(), balance,
-            refiner, maxRefL
-        );
-
-        // Create AMR criterion field
-        volScalarField test
-        (
-            IOobject
-            (
-                "test",
-                runTime.timeName(),
-                mesh,
-                IOobject::NO_READ,
-                IOobject::NO_WRITE,
-                true
-            ),
-            mesh,
-            dimensionedScalar("zero",dimless,0.0)
-        );
-
-        // Set test to 1 inside the refined box
-        forAll(mesh.C(), ci)
-        {
-            const auto& center = mesh.C()[ci];
-            if 
-            (
-                center.x() >= boxBounds[0] && center.x() <= boxBounds[1]
-                && center.y() >= boxBounds[2] && center.y() <= boxBounds[3]
-            )
-            {
-                test[ci] = 1;
-            } else {
-                test[ci] = 0;
-            }
-        }
-
-        for(label i = 0; i < maxRefL; i++)
-        {
-            runTime++;
-            // Update mesh
-            tryAndCatchAbortingCode
-            (
-                [&]()
-                {
-                    mesh.update();
-                }
-            );
-        }
-
-        IStringStream boxIs(boxString);
-        boxToCell box(mesh, boxIs);
-        cellSet boxCells(mesh, "boxCells", IOobject::NO_READ);
-        box.applyToSet(topoSetSource::setAction::NEW, boxCells);
-
-        label origBoxNCells = returnReduce(boxCells.size(), sumOp<label>());
-        label newBoxNCells = -1;
-
-        // nuke test field, this must trigger unrefinement on next mesh update
-        test = 0.0;
         runTime++;
         tryAndCatchAbortingCode
         (
             [&]()
             {
-                mesh.update();
+            mesh.update();
             }
         );
-
-        // Check the refinement box for cell count again
-        IStringStream newBoxIs(boxString);
-        boxToCell newBox(mesh, newBoxIs);
-        cellSet newBoxCells(mesh, "newBoxCells", IOobject::NO_READ);
-        newBox.applyToSet(topoSetSource::setAction::NEW, newBoxCells);
-
-        newBoxNCells = returnReduce(newBoxCells.size(), sumOp<label>());
-
-        // Require that the old box gets unrefined
-        CHECK(newBoxNCells < origBoxNCells);
     }
+
+    // Check the refined box again with a cellSet
+    IStringStream newRefBoxIs(boxString);
+    boxToCell newRefBox(mesh, newRefBoxIs);
+    cellSet newRefBoxCells(mesh, "newRefBoxCells", IOobject::NO_READ);
+    newRefBox.applyToSet(topoSetSource::setAction::NEW, newRefBoxCells);
+
+    newRefBoxNCells = returnReduce(newRefBoxCells.size(), sumOp<label>());
+
+    // Require that the new box gets refined to at least 4xmaxRefLx the original cell count
+    // This is an inaccurate check; but there is nothing we can do.
+    REQUIRE(newRefBoxNCells >= 4 * origRefBoxNCells);
+
+    label newBoxNCells = -1;
+
+    // nuke test field, this must trigger unrefinement on next mesh update
+    test = 0.0;
+    runTime++;
+    tryAndCatchAbortingCode
+    (
+        [&]()
+        {
+          mesh.update();
+        }
+    );
+
+    // Check the refinement box for cell count again
+    IStringStream newBoxIs(boxString);
+    boxToCell newBox(mesh, newBoxIs);
+    cellSet newBoxCells(mesh, "newBoxCells", IOobject::NO_READ);
+    newBox.applyToSet(topoSetSource::setAction::NEW, newBoxCells);
+
+    newBoxNCells = returnReduce(newBoxCells.size(), sumOp<label>());
+
+    // Require that the old box gets unrefined
+    REQUIRE(newBoxNCells < newRefBoxNCells);
+
+    // Reset time for good mesure
+    runTime.setTime(0.0, 0);
+}
+
+TEST_CASE
+(
+    "Check boundary protection for adaptiveFvMesh",
+    "[hex2D][hex3D][poly2D][serial][parallel]"
+)
+{
+    FatalError.throwExceptions();
+    Time& runTime = *timePtr;
+    argList& args = *argsPtr;
+
+    // Tested variables' matrix 
+    word refiner = GENERATE("polyRefiner");
+    word balance = Pstream::parRun() ? GENERATE("no", "yes") : GENERATE("no");
+    label nBufferLayers = GENERATE(1, 2);
+    label maxRefL = GENERATE(3);
+    label nPatchesBuffers = GENERATE(1, 2);
+
+    word caseName = runTime.caseName();
+    word patchName =
+        caseName.starts_with("hex") ? "fixedWalls" : "sides";
+
+    // Supported constant/dynamicMeshDict entries
+    IStringStream is
+    (
+        "dynamicFvMesh   adaptiveFvMesh;"
+        "errorEstimator  fieldValue;"
+        "fieldName       test;"
+        "balance         "+balance+";"
+        "refiner         "+refiner+";"
+        "refineInterval  1;"
+        "unrefineInterval  1;"
+        "balanceInterval  1;"
+        "lowerRefineLevel 0.01;"
+        "unrefineLevel 0.01;"
+        "nBufferLayers   "+Foam::name(nBufferLayers)+";"
+        "maxRefinement   "+Foam::name(maxRefL)+";"
+        "dumpLevel       false;"
+        "protectedPatches ("+patchName+");"
+        "nPatchesBuffers "+Foam::name(nPatchesBuffers)+";"
+    );
+
+    // Reset time
+    runTime.setTime(0.0, 0);
+ 
+    // Create mesh object
+    IOdictionary dynamicMeshDict
+    (
+        IOobject
+        (
+            "dynamicMeshDict",
+            runTime.constant(),
+            runTime,
+            IOobject::NO_READ,
+            IOobject::NO_WRITE
+        ),
+        is
+    );
+    dynamicMeshDict.regIOobject::write();
+    adaptiveFvMesh mesh
+    (
+        IOobject
+        (
+            "",
+            runTime.constant(),
+            runTime,
+            IOobject::MUST_READ,
+            IOobject::NO_WRITE
+        )
+    );
+
+    // Capture test variables for sane reports
+    CAPTURE
+    (
+        Pstream::myProcNo(), Pstream::nProcs(),
+        runTime.caseName(),
+        mesh.nCells(), balance,
+        refiner, maxRefL, nBufferLayers, nPatchesBuffers
+    );
+
+    // Create AMR criterion field
+    volScalarField test
+    (
+        IOobject
+        (
+            "test",
+            runTime.timeName(),
+            mesh,
+            IOobject::NO_READ,
+            IOobject::NO_WRITE,
+            true
+        ),
+        mesh,
+        dimensionedScalar("zero",dimless,1.0)
+    );
+
+    label initNCells = returnReduce(mesh.nCells(), sumOp<label>());
+
+    // run a refinement cycle, minor unrefinement should happen here, but this not checked.
+    for(label i = 0; i < maxRefL; i++)
+    {
+        runTime++;
+        // Update mesh
+        tryAndCatchAbortingCode
+        (
+            [&]()
+            {
+              mesh.update();
+            }
+        );
+    }
+
+    label finalNCells = returnReduce(mesh.nCells(), sumOp<label>());
+    REQUIRE(finalNCells > initNCells);
 
     // Reset time for good mesure
     runTime.setTime(0.0, 0);
