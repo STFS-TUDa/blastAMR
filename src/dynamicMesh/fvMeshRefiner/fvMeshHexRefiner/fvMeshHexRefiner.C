08d57a43d97b9a4e4dc3139fbe7ea2fa29205caf
Elwardi <elwardi.fadeli@tu-darmstadt.de>
2025-11-28 13:54:18 +0100

    refactor: use coreAMR for the dynamicFvMesh classes route

--- src/dynamicMesh/fvMeshRefiner/fvMeshHexRefiner/fvMeshHexRefiner.C
+++ src/dynamicMesh/fvMeshRefiner/fvMeshHexRefiner/fvMeshHexRefiner.C
@@ -187,6 +187,11 @@ Foam::fvMeshHexRefiner::refine
     // Play refinement commands into mesh changer.
     meshCutter_->setRefinement(cellsToRefine, meshMod);
 
+    // Clear moving flag. This is required since geometry calculation
+    // might get triggered when doing processor patches.
+    // (same as dynamicRefineFvMesh::refine)
+    mesh_.moving(false);
+
     // Create mesh (with inflation), return map from old to new mesh.
     //autoPtr<mapPolyMesh> map = meshMod.changeMesh(mesh_, true);
     autoPtr<mapPolyMesh> map = meshMod.changeMesh(mesh_, false);
@@ -219,6 +224,9 @@ Foam::fvMeshHexRefiner::refine
     //    cellTreePtr_.clear();
 
     // Update fields
+    // Note: mesh_.updateMesh(map) internally calls mapClouds() which handles
+    // cloud remapping. We do NOT call fvMeshRefiner::updateMesh here as that
+    // would result in double cloud remapping.
     mesh_.updateMesh(map);
 
     // Update numbering of protectedCell_
@@ -262,6 +270,10 @@ Foam::fvMeshHexRefiner::unrefine
     Map<label> faceToSplitPoint(0);
     meshCutter_->calcFaceToSplitPoint(splitElems, faceToSplitPoint);
 
+    // Clear moving flag. This is required since geometry calculation
+    // might get triggered when doing processor patches.
+    // (same as dynamicRefineFvMesh::unrefine)
+    mesh_.moving(false);
 
     // Change mesh and generate map.
     //autoPtr<mapPolyMesh> map = meshMod.changeMesh(mesh_, true);
@@ -273,6 +285,9 @@ Foam::fvMeshHexRefiner::unrefine
         << endl;
 
     // Update fields
+    // Note: mesh_.updateMesh(map) internally calls mapClouds() which handles
+    // cloud remapping. We do NOT call fvMeshRefiner::updateMesh here as that
+    // would result in double cloud remapping.
     mesh_.updateMesh(map);
 
     // Update numbering of protectedCell_
