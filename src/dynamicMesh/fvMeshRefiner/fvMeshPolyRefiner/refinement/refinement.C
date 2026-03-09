99938626b3a97cde4c54b98238cce80b4f9f2242
Elwardi <e.fadeli@ianus-simulation.de>
2026-03-09 09:19:32 +0100

    fix: refinement history from updateMesh

--- src/dynamicMesh/fvMeshRefiner/fvMeshPolyRefiner/refinement/refinement.C
+++ src/dynamicMesh/fvMeshRefiner/fvMeshPolyRefiner/refinement/refinement.C
@@ -1037,6 +1037,13 @@ void Foam::refinement::updateMesh(const mapPolyMesh& map)
             // Assume it is after hexRef that this routine is called.
             hexRef::reorder(reversePointMap, mesh_.nPoints(), -1,  pointLevel_);
         }
+        else if (pointLevel_.size() == mesh_.nPoints())
+        {
+            // pointLevel_ was already updated to the new mesh size
+            // by setRefinement() with correct levels for new midpoints.
+            // No remapping needed since changeMesh preserves the
+            // polyTopoChange ordering.
+        }
         else
         {
             // Map data
